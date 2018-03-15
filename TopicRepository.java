13981deb0cc7d0ff897515cc4754761ba24326b2
SBathio <33559437+SBathio@users.noreply.github.com>
2018-03-15 17:44:08 -0400

    Delete TopicRepository.java

--- TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package springbootStarterRevision.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String> {
-	
-	
-
-}
