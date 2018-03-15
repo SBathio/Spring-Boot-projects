a608a94de1eef4486421648e424552f16ef0c4d9
SBathio <33559437+SBathio@users.noreply.github.com>
2018-03-15 17:37:38 -0400

    Add files via upload

---
+++ CourseApiDataRevisionApplication.java
@@ -0,0 +1,12 @@
+package springbootStarterRevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiDataRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiDataRevisionApplication.class, args);
+	}
+}
